a7e8b04904dd0b85a8d1fbcb06fe05e55bd8e346
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-06-23 21:41:17 +0530

    new file added

---
+++ class5.py
@@ -0,0 +1,25 @@
+''' Loops - When we want to perform repititive task we use loops.
+Types of loops -
+For loop, While loop 
+For loop -  it is used when you know till where you wanna go 
+While loop - it is used with a cindition.'''
+
+for i in range(1,41,1):
+    print(i) 
+
+for i in range(-10,21,1):
+    print(i)
+
+for i in range(35,4,-1):
+    print(i)
+
+for i in range(5,51,5):
+    print(i)    
+    
+    
+a = "Nature"
+
+for i in range(len(a)):
+    print(a[i])
+
+
