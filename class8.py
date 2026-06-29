25362acb1a0d768098b8b3a47de09fd3293b2f29
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-06-29 17:55:06 +0530

    Data structure in python

---
+++ class8.py
@@ -0,0 +1,44 @@
+#  Datastructure in python--
+
+"""Data structure are a type of storage in which we can store multiple values.
+
+in-built data structures--
+
+List, Tuple , Dictionary , Set  
+
+Customized Data structure--
+
+stack, Queue, Linked List, Trees, Graphs..."""
+
+# Some example of data structure--
+
+a = [12,13,14] # List
+
+a = (12,45,67) # Tuple
+
+a = {13,15,90} # Set
+
+# List----(how to create list)
+
+a = [1,1.4,12j,"hello",True,print(),str()]
+
+# you can store anything inside a list 
+# hetrogeneous Nature
+
+# list can also store duplicates
+
+b = [1,1,1,1,1,2,2,2,2,3,3,3,3]
+
+# List are mutable that can you can change anything
+
+c = [1,2,3,4.5,5,6,7]
+# you can change 4.5 to 4 cause it is mutable
+
+
+
+
+
+
+
+
+
