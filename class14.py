fb7ae5b89e31c696ac9a540b3d39bb42802bd818
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-05 17:34:39 +0530

    file handling file added

---
+++ class14.py
@@ -0,0 +1,14 @@
+# File handling----
+
+# file= open("class6.py")  
+# print(file.read())
+
+# functions of file handling--
+
+# open("pull.txt",'x')
+
+file = open("push.txt",'w')
+
+file.write("hello this is sample file")
+
+file.close()
