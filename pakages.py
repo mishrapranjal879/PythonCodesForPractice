2fa14c4ff557c0a4b45942438206dba7927b5265
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-18 21:45:10 +0530

    pakages in python

---
+++ pakages.py
@@ -0,0 +1,15 @@
+# pakages and modules in python----
+
+# build in modules--
+
+# import math
+# import random
+# import string
+
+# A modules is a simple python file (.py)
+# it contains code -- functions, classes , variables , that you can reuse.
+
+# pakage----
+# numpy
+# pandas
+# matplotlib
