6881f92981abaaa902e4db9e4b80d21fc8a2ff06
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-19 13:19:48 +0530

    visulization of data in to different charts

---
+++ statistical/visualization.py
@@ -0,0 +1,14 @@
+# Visualization----
+
+# It is work on plots.  
+
+# Data
+# BAR chart
+# PIE chart
+# Line graph
+# O -Gives
+# Tow - way table
+# Dot Plot
+# Relative frequency Table - (Formula = Frequency/Total*100)
+# Join Distribution table(it is dependent on whole data)
+# Histogram
