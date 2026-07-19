d1dbd09f44d0ea71fbc5b3d614711ac559a1d9cf
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-19 17:43:28 +0530

    find mean , median , and mode

---
+++ statistical/stats&probability.py
@@ -0,0 +1,13 @@
+# Analyzing Data----
+# Measure of Central Tendency--(it is use to find the middle value and typical value of the data)
+# There are several way to find middle --
+# 1. Mean
+# 2. Median
+# 3. Mode
+
+
+# Median = The middle value when all data is arranged in order.
+
+# Mode = Mode is the number (or category) that appears most often in a dataset.
+
+
