88ca3c054903a8fc5a503761dd82f809f47d3e1b
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-20 23:43:29 +0530

    variance and standard deviation

---
+++ statistical/data&distribution.py
@@ -0,0 +1,20 @@
+# Data and Distribution---
+# (Variance , standard , deviation)
+
+""" Data distribution tells how often each value 
+(or range of values) appears in our dataset."""
+
+#  Types of distribution---
+# 1. Normal Distribution(Bell-Shaped Curve)
+
+# 2. Skewed Distribution
+
+# Varience = "how far the values are from the mean."
+# if variance is small - values are close the mean.
+# if variance is big - values are spread out.
+
+# Standard deviation :-
+# it is the squre root of variance.
+# it tells us on average how far each value is form the mean,
+# in the same unit as the data.
+
