a827fd20e760aea250350e412486398e0ee0229d
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-20 19:00:32 +0530

    removing outliers

---
+++ statistical/outliers.py
@@ -0,0 +1,13 @@
+#  Outliers---
+
+"""an outlir is a value in your
+ data that is much smaller or much larger than 
+ most of the other values"""
+
+# 1,2,3,4,6,7,110 = 110 is outlier
+
+# Remmoving the outliers---
+
+# Upper fence = Q3 + 1.5(IQR)
+# Lower fence = Q1 - 1.5(IQR)
+
