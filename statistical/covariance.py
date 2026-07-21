55d60425fad509bf2194cc7eb496a19317e3f6d0
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-21 18:05:32 +0530

    covariance and correlation

---
+++ statistical/covariance.py
@@ -0,0 +1,30 @@
+# Covariance and coorelation---
+
+"""Definition:
+Covariance is a statistical measure that indicates how two variables change together.
+
+If both variables increase or decrease together, the covariance is positive.
+If one variable increases while the other decreases, the covariance is negative.
+If there is no consistent relationship, the covariance is close to zero.
+
+Formula:
+
+Cov(X,Y)=n−1∑i=1n​(Xi​−Xˉ)(Yi​−Yˉ)​/ n - 1
+"""
+
+# correlation----
+
+"""2. Correlation
+
+Definition:
+Correlation is a statistical measure that describes both the strength and direction of the relationship between two variables.
+
+Correlation values always lie between -1 and +1.
++1 → Perfect positive relationship
+0 → No linear relationship
+-1 → Perfect negative relationship
+
+Formula (Pearson Correlation):
+
+r=σX​σY​Cov(X,Y)​/standard deviation of X and Y
+"""
