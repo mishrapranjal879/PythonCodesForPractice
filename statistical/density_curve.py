2606a028c37960ea90f1cf99704654998823717d
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-21 15:41:49 +0530

    density curve and emperical rule

---
+++ statistical/density_curve.py
@@ -0,0 +1,16 @@
+# Density curve and Emperical Rule--
+
+""" A density curve is just a smooth picture of data
+distribution that helps us see the pattern of data clearly."""
+
+# Emperical rule---
+
+# in a normal distribution (bell curve):
+
+# . About 68% of the data lies within 1 standard deviation of the mean.
+
+# . About 95% lies within 2 standard deviations .
+
+# . About 99.7% lies within 3 standard deviations .
+
+
