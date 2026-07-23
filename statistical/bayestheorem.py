80091c8a24d99313e8e1559e64374653210f678b
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-23 20:47:32 +0530

    bayes theorem

---
+++ statistical/bayestheorem.py
@@ -0,0 +1,9 @@
+# Bayes Theorem---
+
+""" Bayes' Theorem is a probability theorem used to calculate the probability of an event based on prior knowledge of related events. """
+
+# Meaning of the Terms
+# P(A∣B) = Probability of event A occurring given that B has occurred (Posterior Probability).
+# P(B∣A) = Probability of event B occurring given that A has occurred (Likelihood).
+# P(A) = Probability of event A before considering B (Prior Probability).
+# P(B) = Total probability of event B occurring.
