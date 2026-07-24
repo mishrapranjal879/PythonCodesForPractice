cada37f474c334e6e55f70b43f8e23552e218cd4
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-24 21:33:16 +0530

    hypothesis testing

---
+++ statistical/hypothesistesting.py
@@ -0,0 +1,6 @@
+# Hypothesis testing----
+
+""" hypothesis testing is like a decision-making framework in statistic.
+We take data feom sample, then test if what we see is really true for the whole population."""
+
+ 
