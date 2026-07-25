594457d68678ee5eb227e069fdb958161cf272dd
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-25 21:22:37 +0530

    Significance lavel and errors

---
+++ statistical/levels&errors.py
@@ -0,0 +1,12 @@
+# Significance level and Errors---
+
+""" The significance lavel(alpha ) is the cut off point bwe use to decide whether to reject null hypothesis"""
+
+# it tells how much chance we are willing to take to make a wrong decision.
+
+# Type1 error -(We reject the null hypothesis in reality it was True)
+# Type 2 error -(We accept the null hypothesis in reality it was false)
+
+# 1. One tailed test
+# 2. Two tailed test
+# Test statistics
