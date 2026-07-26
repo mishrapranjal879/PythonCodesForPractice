f794f086fea797b35ec24122dca6b2723f088c4b
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-26 21:23:24 +0530

    z-test in hypothesis

---
+++ statistical/z-test.py
@@ -0,0 +1,13 @@
+# Z - Test---
+""" A Z-Test is a statistical hypothesis test used to determine whether there
+ is a significant difference between a sample mean and a population mean, or between the means/proportions 
+of two samples, when the population standard deviation is known and the sample size is large."""
+
+""" When is a Z-Test Used?
+
+A Z-Test is appropriate when:
+
+The population standard deviation (σ) is known.
+The sample size is 30 or more (n ≥ 30), or the population is normally distributed.
+The sample is selected randomly.
+Observations are independent."""
