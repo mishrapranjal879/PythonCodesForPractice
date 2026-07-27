7c58c640d837c076041e6f562c923b86b51f5c55
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-27 20:15:27 +0530

    z-test questions

---
+++ statistical/practice1.py
@@ -0,0 +1,23 @@
+# Question 2: When is a Z-Test used?
+
+# Answer:
+# A Z-Test is used when:
+
+# Population standard deviation is known.
+# Sample size is greater than or equal to 30.
+# Data are approximately normally distributed.
+# Samples are selected randomly.
+
+
+# Question 4: What is the null hypothesis (H₀) in a Z-Test?
+
+# Answer:
+# The null hypothesis states that there is no significant difference between the sample mean and the population mean.
+
+# Example:
+
+# H
+# 0
+# 	​
+
+# :μ=50
