b77a7bb3474c7e3d9210c8f1b86b5bdac37f7690
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-07-31 20:30:32 +0530

    new file added

---
+++ statistical/f-test.py
@@ -0,0 +1,7 @@
+# # F-Test (Fisher's Test)
+
+# The F-Test is a statistical test used to compare variances (variability) of two or more populations. It is also widely used in ANOVA (Analysis of Variance) to determine whether the means of several groups are significantly different.
+
+# Definition
+
+# An F-Test is a hypothesis test in which the test statistic follows the F-distribution under the null hypothesis.
