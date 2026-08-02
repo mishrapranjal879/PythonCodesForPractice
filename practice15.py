ceaddf0071069f6cf33f60f3f278298890968606
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-08-02 20:44:42 +0530

    new practiceset added

---
+++ practice15.py
@@ -0,0 +1,29 @@
+# 1. Create a NumPy array from 1 to 10
+# Question
+
+# Create a NumPy array containing numbers from 1 to 10.
+
+# Solution
+# import numpy as np
+
+# arr = np.arange(1, 11)
+
+# print(arr)
+
+
+# 2. Create a 3×4 array of zeros
+# Solution
+# import numpy as np
+
+# arr = np.zeros((3, 4))
+
+# print(arr)
+
+
+# 3. Create a 4×4 array of ones
+# Solution
+# import numpy as np
+
+# arr = np.ones((4, 4))
+
+# print(arr)
