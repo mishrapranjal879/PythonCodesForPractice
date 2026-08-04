2dcd42aa10b1fe458712386b9ba5900dd519622b
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-08-04 20:27:56 +0530

    pandas librery in python

---
+++ pandas.py
@@ -0,0 +1,18 @@
+# # Pandas ------
+
+
+# Pandas – Definition
+
+# Pandas is an open-source Python library used for data manipulation, analysis, and cleaning. It provides fast, flexible, and easy-to-use data structures such as Series (1-dimensional) and DataFrame (2-dimensional) to work with structured data like tables, spreadsheets, and CSV files.
+
+# Simple Definition
+
+# Pandas is a Python library used to read, organize, clean, analyze, and manipulate data efficiently.
+
+# Key Features
+# Reads data from CSV, Excel, SQL, JSON, etc.
+# Cleans and handles missing data.
+# Filters, sorts, and groups data.
+# Performs data analysis and statistical operations.
+# Merges and joins multiple datasets.
+# Exports data to various file formats.
