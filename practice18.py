c198ae025bd6e64540fd5db444e3674736ca6eac
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-08-09 23:18:44 +0530

    new file added

---
+++ practice18.py
@@ -0,0 +1,22 @@
+# # 1. Create a Pandas Series
+
+# Question: Create a Series from [10, 20, 30, 40, 50].
+
+# import pandas as pd
+
+# s = pd.Series([10, 20, 30, 40, 50])
+
+# 2. Create a DataFrame
+
+# Question: Create a DataFrame containing Name, Age and Marks.
+
+# import pandas as pd
+
+# data = {
+#     "Name": ["Pranjal", "Rahul", "Amit", "Neha"],
+#     "Age": [21, 22, 20, 21],
+#     "Marks": [85, 78, 92, 88]
+# }
+
+# df = pd.DataFrame(data)
+# print(df)
