eae8a6c3ddfb1f4bb8b7ecd0225d1593222f6a2b
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-08-15 21:29:07 +0530

    matplotlib in python

---
+++ vizualization_library.py
@@ -0,0 +1,50 @@
+# # # 1. Import Matplotlib
+
+# # # Question: How do you import Matplotlib's pyplot module?
+
+# # # Solution:
+
+# # # import matplotlib.pyplot as plt
+
+# # 2. Simple Line Plot
+
+# # Question: Create a line graph for the following data:
+
+# # x = [1, 2, 3, 4, 5]
+# # y = [10, 20, 15, 30, 25]
+
+# # Solution:
+
+# # import matplotlib.pyplot as plt
+
+
+# # x = [1, 2, 3, 4, 5]
+# # y = [10, 20, 15, 30, 25]
+
+
+# # plt.plot(x, y)
+# # plt.show()
+
+
+# 3. Add Title and Labels
+
+# Question: Create a line plot and add a title, x-axis label, and y-axis label.
+
+# Solution:
+
+# import matplotlib.pyplot as plt
+
+
+# x = [1, 2, 3, 4, 5]
+# y = [10, 20, 15, 30, 25]
+
+
+# plt.plot(x, y)
+
+
+# plt.title("Sales Report")
+# plt.xlabel("Days")
+# plt.ylabel("Sales")
+
+
+# plt.show()
