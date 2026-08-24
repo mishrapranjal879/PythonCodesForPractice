e8c06a869ce810651b26ef0de87d8c88dc28280e
mishrapranjal879 <mishrapranjal879@gmail.com>
2026-08-24 20:59:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+print("namastey youtube i am learning python")
+a= 12/3
+print(type(a))
+c= 4
+print(type(c))
+# String
+A= "Pranjal"
+print(A)
+print(type(A))
+
+b = True
+print(type(b))
+# How string are work
+
+r= "R"
+print(ord(r))
+
+t= "PRANJAL"
+print(t[-1],t[3])  
+# String slicing
+y= "PRANJAL MISHRA"
+
+print(y[0:14:1])
+
+#Type conversion
+w= "12"
+
+w= int(w)
+print(type(w))
+
+# Boolean
+
+e= 12
+
+print(bool(e))
+
+# When we give a empty strig it will print only false
+# ex:
+
+d= 0
+print(bool(d)) # give only false 
+
+ # Format string
+name = "Pranjal mishra"
+age = "23"
+
+print(f"my name is {name} and my age is {age}")
+
+
+
+# Operators 
+# Aritmetic opertaor 
+q= 5
+o = 20
+# print(q+o)
+# print(o-q)
+# print(o/q)
+#print(o//q)
+#print(q*o)
+# print(20%5)
+#print(5**2)
+#print(5**3)
+#print(32%5)  # For find remainder 
+
+print(12+4/2)
+
+# Assignment operators
+a = 23
+print(a+20)
+#compound assignment operators
+a+= 22
+a+= 30
+print(a)
